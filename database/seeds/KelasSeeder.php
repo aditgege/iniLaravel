d44fa74a12682ec2d902b104990e1e131a5c723a
aditgege <aditgege0@gmail.com>
2017-09-20 18:24:05 +0700

    versi 1.0.0

---
+++ database/seeds/KelasSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KelasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+        		'nama_kelas'=>'XII-RPL3',
+        		'jurusan'	=>'Rekayasa perangkat Lunak'
+        ];
+
+        DB::table('t_kelas')->insert($data);
+
+        $data = [
+        		'nama_kelas'=>'XII-RPL1',
+        		'jurusan'	=>'Rekayasa perangkat Lunak'
+        ];
+         DB::table('t_kelas')->insert($data);
+    }
+}
